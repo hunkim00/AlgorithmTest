017a7c01b7ff0106c5446fa552e40bb612082dfa
hunkim00 <kimhun1027@naver.com>
2022-11-03 12:28:09 +0900

    AlgorithmTest

---
+++ src/main/java/com/algorithm/algorithmtest/AlgorithmTestApplication.java
@@ -0,0 +1,13 @@
+package com.algorithm.algorithmtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/algorithm/algorithmtest/AlgorithmTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorithm.algorithmtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
